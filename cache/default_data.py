14324ecdab0cac112e3d9ef3ce0c98df5257fc79
Ageel <Ageal.atared@gmail.com>
2026-09-11 14:08:06 +0200

    Add files via upload

---
+++ cache/default_data.py
@@ -0,0 +1,265 @@
+# default lectures
+lectures = [
+{
+                "id": 7,
+                "title": "شكرا لثبيت التطبيق ونتمنى نيل إعجابكم",
+                "member_id": 0,
+                "member_name": "بلازما للبرمجيات",
+                "member_img": "plasma.png",
+                "date": "2026-01-01",
+                "completed_at": "01-01-2026",
+                "type": "شكر خاص",
+                "finished": "false",
+                "votes": 18,
+                "suggested_by": "بلازمــــــــــــــا",
+                "locked": "false",
+                "summary": ""
+            },
+{
+                "id": 6,
+                "title": "التفكك الأسري",
+                "member_id": 1,
+                "member_name": "إسماعيل شريف",
+                "member_img": "1.png",
+                "date": "2026-09-11",
+                "completed_at": "11-09-2026",
+                "type": "إجتماعي",
+                "finished": "true",
+                "votes": 10,
+                "suggested_by": "أعضاء الملتقى",
+                "locked": "true",
+                "summary": ""
+            },
+            {
+                "id": 5,
+                "title": "العمل التطوعي",
+                "member_id": 10,
+                "member_name": "الصادق إمام",
+                "member_img": "10.png",
+                "date": "2026-09-4",
+                "completed_at": "04-09-2026",
+                "type": "علم الإجتماع",
+                "finished": "true",
+                "votes": 10,
+                "suggested_by": "أعضاء الملتقى",
+                "locked": "true",
+                "summary": ""
+            },
+              {
+                "id": 4,
+                "title": "النوم والأحلام من وجهة نظر الفسيولوجيا",
+                "member_id": 3,
+                "member_name": "عبد القادر سراج",
+                "member_img": "3.png",
+                "date": "2026-08-28",
+                "completed_at": "28-08-2026",
+                "type": "علم النفس",
+                "finished": "true",
+                "votes": 10,
+                "suggested_by": "أعضاء الملتقى",
+                "locked": "true",
+                "summary": ""
+            },
+            {
+                "id": 3,
+                "title": "تحويل الفكرة إلى فرصة",
+                "member_id": 7,
+                "member_name": "أحمد قصدير",
+                "member_img": "7.png",
+                "date": "2026-08-21",
+                "completed_at": "21-08-2026",
+                "type": "ريادة أعمال",
+                "finished": "true",
+                "votes": 10,
+                "suggested_by": "أعضاء الملتقى",
+                "locked": "true",
+                "summary": ""
+            },
+{
+                "id": 2,
+                "title": "الإستلاب الثقافي وأثره على الفرد والمجتمع",
+                "member_id": 18,
+                "member_name": "محي الدين الهادي",
+                "member_img": "18.png",
+                "date": "2026-08-14",
+                "completed_at": "14-08-2026",
+                "type": "إجتماعي",
+                "finished": "true",
+                "votes": 10,
+                "suggested_by": "أعضاء الملتقى",
+                "locked": "true",
+                "summary": ""
+            },
+            {
+                "id": 1,
+                "title": "التفكير الإيجابي",
+                "member_id": 4,
+                "member_name": "قدورة صديق",
+                "member_img": "4.png",
+                "date": "2026-08-7",
+                "completed_at": "07-08-2026",
+                "type": "علم النفس",
+                "finished": "true",
+                "votes": 10,
+                "suggested_by": "أعضاء الملتقى",
+                "locked": "true",
+                "summary": ""
+            }
+]
+
+# default topics
+topics = [
+{
+            "id": 18,
+            "title": "شكرا لتثبيت التطبيق ونتمنى نيل إعجابكم",
+            "suggested_by":"بلازما للبرمجيات",
+            "category": "شكر خاص",
+            "votes": 18,"accepted": "true", "comments_count": 0,
+            "date":"01-01-2026"
+            },
+            {
+            "id": 17,
+            "title": "الطلاق وأثرھا على أفراد الأسرة ",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علم النفس",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"13-9-2026"
+            },
+            {
+            "id": 16,
+            "title": "الترابط الإجتماعي",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علم الإجتماع",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"13-9-2026"
+            },
+            {
+            "id": 15,
+            "title": "رحلة إلى المجهول: ماذا نعرف حقاً عن عالم الجان والشياطين",
+            "suggested_by":"",
+            "category": "ماورائيات",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"11-9-2026"
+            },
+            {
+            "id": 14,
+            "title": "الزواج مفھومھا ومعاييرھا",
+            "suggested_by":"ملتقى الفكر",
+            "category": "تربوي",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"9-9-2026"
+            },
+            {
+            "id": 13,
+            "title": "مبادئ الإعلام",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علوم الإتصال",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"8-9-2026"
+            },
+            {
+            "id": 12,
+            "title": "فن الخطابة",
+            "suggested_by":"ملتقى الفكر",
+            "category": "لغويات",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"6-9-2026"
+            },
+            {
+            "id": 11,
+            "title": "وكذلك البرمجة",
+            "suggested_by":"قدورة صديق",
+            "category": "تقني",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"1-9-2026"
+            },
+            {
+            "id": 10,
+            "title": "الرومانسية وأھميتھا في الحياة الزوجية",
+            "suggested_by":"ملتقى الفكر",
+            "category": "عاطفي",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"31-8-2026"
+            },
+            {
+            "id": 9,
+            "title": "فن التعامل بين الأزواج",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علاقات زوجية",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"30-8-2026"
+            },
+            {
+            "id": 8,
+            "title": "إدارة المنظمات والعمل الطوعي",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علوم إدارية",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"29-8-2026"
+            },
+            {
+            "id": 7,
+            "title": "التخطيط الإستراتيجي",
+            "suggested_by":"ملتقى الفكر",
+            "category": "إدارة أعمال",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"27-8-2026"
+            },
+            {
+            "id": 6,
+            "title": "إدارة الموارد البشرية",
+            "suggested_by":"ملتقى الفكر",
+            "category": "إدارة أعمال",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"25-8-2026"
+            },
+            {
+            "id": 5,
+            "title": "حسن الظن",
+            "suggested_by":"ملتقى الفكر",
+            "category": "إسلامي",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"22-8-2026"
+            },
+            {
+            "id": 4,
+            "title": "فن الحوار",
+            "suggested_by":"ملتقى الفكر",
+            "category": "تنمية بشرية",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"20-8-2026"
+            },
+            {
+            "id": 3,
+            "title": "فض النزاعات",
+            "suggested_by":"ملتقى الفكر",
+            "category": "سياسي",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"19-8-2026"
+            },
+            {
+            "id": 2,
+            "title": "حسن التعليل أو التبرير",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علم النفس",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"17-8-2026"
+            },
+            {
+            "id": 1,
+            "title": "العمل النقابي",
+            "suggested_by":"ملتقى الفكر",
+            "category": "حقوق وحريات",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"11-8-2026"
+            },
+                {
+            "id": 0,
+            "title": "علاقات عامة",
+            "suggested_by":"ملتقى الفكر",
+            "category": "علوم إدارية",
+            "votes": 0,"accepted": "false", "comments_count": 0,
+            "date":"9-8-2026"
+            },
+                        
+        ]
